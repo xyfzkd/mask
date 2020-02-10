94d0dd455f5692fcd16fafe9da318bc2f34db6c1
YufengXie <xyfzkd@outlook.com>
2020-02-10 15:01:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@
 @LastEditors  : Xie Yufeng
 @LastEditTime : 2020-02-04 15:40:45
 '''
-USERNAME = "谢孝龙"
-USERIC = "330622196503214831"
-PHONE = "18957546582"
+USERNAME = "**"
+USERIC = "**"
+PHONE = "**"
 #Social security card
-SSC = "E1836581X"
+SSC = "**"
 EARE = "上虞区"
 # recieving address
-RA = "道墟镇联江村662号"
+RA = "**"
 
 
